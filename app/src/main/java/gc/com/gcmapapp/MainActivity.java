13f55cd525e84847409da5248a6d287a6838c660
guijunji <694362383@qq.com>
2017-12-05 17:38:54 +0800

    添加百度sdk

---
+++ app/src/main/java/gc/com/gcmapapp/MainActivity.java
@@ -0,0 +1,52 @@
+package gc.com.gcmapapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.bmapView)
+    MapView bmapView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        bmapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        bmapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        bmapView.onPause();
+    }
+
+
+}
+
